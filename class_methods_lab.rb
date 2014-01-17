87cef9d72550061dfde70c0788020a0bd7591895
Tom Dyer <tdyer@ourstage.com>
2014-01-17 10:12:10 -0500

    updated lab

--- class_methods_lab.rb
+++ class_methods_lab.rb
@@ -1,5 +1,5 @@
 # Lab
-# 1) Create a class song that has a name, url and 
+# 1) Create a class song that has a name, url and
 # price attributes.
 
 # i) The name can not be changed
@@ -12,8 +12,19 @@
 #  that will give me a total price of all the songs handed to it.
 
 # iv) Create a Tax module that contains the flat tax rates,
-# a percentage, for all the New England states. 
-# (Make up this flat tax rate if you want)
+# a percentage, for all the New England states.
+# and it will also have a Federal tax rate of 3 percent.
+# (Make up these state tax rate if you want)
 
-# v) Mix in the Tax module to the Song class and 
+# v) Change the Song#price method so that it will take
+# a state argument. This will allow us to give the price
+# after that state and federal taxes have been applied.
+
+# v) Mix in the Tax module to the Song class and
 # use its rate method ONLY if the Song#price method.
+
+# vi) Create Discount module. This will provide a discount
+# 10% for more that 5 songs and 20% for more than 10 songs.
+
+# vii) Mix in the Discount module into the Song class and
+# and use it to calculate the total price of all the songs.
