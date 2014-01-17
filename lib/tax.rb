2c5f832b1b5ae82a6d908c2ba4d05badfe2ab560
Tom Dyer <tdyer@ourstage.com>
2014-01-17 08:13:00 -0500

    added federal taxes

--- lib/tax.rb
+++ lib/tax.rb
@@ -1,7 +1,8 @@
-module StateTax
+module Tax
   TaxTable = {MA: 5.2, CONN: 6.2, NH: 0.0, VT: 5.2, ME: 5.7 }
+  FED_RATE = 3.2
 
-  def tax_rate(state)
+  def state_rate(state)
     TaxTable[state.to_sym]
   end
 
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,29 +1,6 @@
 require_relative 'spec_helper'
 require_relative '../lib/song'
 
-# Lab
-# 1) Create a class song that has a name, url and 
-# price attributes.
-
-# i) The name can not be changed
-
-# ii) The URL and price can be changed
-
-# ii) Create 5 songs objs.
-
-# iii) Create a class method, Songs.total_price(*songs),
-#  that will give me a total price of all the songs handed to it.
-
-# iv) Create a Tax module that contains the flat tax rates,
-# a percentage, for all the New England states. 
-# (Make up this flat tax rate if you want)
-
-# v) Mix in the Tax module to the Song class and 
-# use its rate method ONLY if the Song#price method.
-
-# vi) Mix in the Tax module to the Song class and 
-# use its rate method ONLY if the Song.total_price method.
-
 describe Song do 
 
   it "#new" do
@@ -45,7 +22,7 @@ describe Song do
   end
 
   it ".total_price" do
-    expect(Song.total_price(songs)).to eq 5.20
+    expect(Song.total_price(songs, "MA")).to eq 4.7632
   end
 
 end
