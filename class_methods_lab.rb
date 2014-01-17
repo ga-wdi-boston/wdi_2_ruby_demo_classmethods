7676cc3c202de152d8ec79a1fdb6bff91192c997
Tom Dyer <tdyer@ourstage.com>
2014-01-17 07:43:13 -0500

    initial commit

---
+++ class_methods_lab.rb
@@ -0,0 +1,19 @@
+# Lab
+# 1) Create a class song that has a name, url and 
+# price attributes.
+
+# i) The name can not be changed
+
+# ii) The URL and price can be changed
+
+# ii) Create 5 songs objs.
+
+# iii) Create a class method, Songs.total_price(*songs),
+#  that will give me a total price of all the songs handed to it.
+
+# iv) Create a Tax module that contains the flat tax rates,
+# a percentage, for all the New England states. 
+# (Make up this flat tax rate if you want)
+
+# v) Mix in the Tax module to the Song class and 
+# use its rate method ONLY if the Song#price method.
